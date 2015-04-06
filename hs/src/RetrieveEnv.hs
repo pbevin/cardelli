c8c4e4efa4e5807f3686ca221765c22729cbde87
Pete Bevin <pete@petebevin.com>
2015-04-05 20:03:37 -0400

    Refactor copyType and friends

--- hs/src/RetrieveEnv.hs
+++ hs/src/RetrieveEnv.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module RetrieveEnv where
 
 import Control.Monad.State
+import Control.Applicative
 import Type
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -11,35 +14,41 @@ retrieveEnv a tv = do
   case Map.lookup a (vars tv) of
     Nothing -> return Nothing
     Just t' -> do
-      copied <- copyType t' (ngvars tv)
+      env <- get
+      let (copied, newEnv) = runCopier (copyType t' (ngvars tv)) env
+      put newEnv
       return $ Just copied
-
-
-copyType :: Type -> NonGenericVars -> State Env Type
-copyType t ngvars = do
-  (t, _) <- copyType' t ngvars Map.empty
-  return t
 
 type CopyMap = Map TypeName Type
 
-copyType' :: Type -> NonGenericVars -> CopyMap -> State Env (Type, CopyMap)
-copyType' t@(TypeVariable id) ngvars copyMap =
-  if id `elem` ngvars
-  then return (t, copyMap)
-  else do
-    case Map.lookup id copyMap of
-      Nothing -> do
-        t' <- newVar
-        return (t', Map.insert id t' copyMap)
-      Just t' -> return (t', copyMap)
-copyType' t@(BasicType _) _ copyMap = return (t, copyMap)
-copyType' t@(TypeOperator op args) ngvars copyMap = do
-  (args', copyMap') <- copyArgs args ngvars copyMap
-  return (TypeOperator op args', copyMap')
+newtype Copier a = Copier {
+  runCM :: StateT CopyMap (State Env) a
+} deriving (Monad, MonadState CopyMap, Functor, Applicative)
 
-copyArgs :: [Type] -> NonGenericVars -> CopyMap -> State Env ([Type], CopyMap)
-copyArgs [] ngvars copyMap = return ([], copyMap)
-copyArgs (t:ts) ngvars copyMap = do
-  (t', cm') <- copyType' t ngvars copyMap
-  (ts', cm'') <- copyArgs ts ngvars cm'
-  return (t':ts', cm'')
+runCopier :: Copier a -> Env -> (a, Env)
+runCopier a env = (t, e')
+  where ((t, _), e') = runState (runStateT (runCM a) Map.empty) env
+
+createType :: Copier Type
+createType = Copier (lift newVar)
+
+copyType :: Type -> NonGenericVars -> Copier Type
+copyType t ngvars = case t of
+  TypeVariable id
+    | id `elem` ngvars -> return t
+    | otherwise        -> copyTypeVariable id
+  BasicType _ -> return t
+  TypeOperator op args -> copyArgs args ngvars >>= return . TypeOperator op
+
+copyTypeVariable :: TypeName -> Copier Type
+copyTypeVariable id = do
+  copyMap <- get
+  maybe (createIn copyMap) return $ Map.lookup id copyMap
+    where createIn copyMap = do
+            t <- createType
+            put $ Map.insert id t copyMap
+            return t
+
+copyArgs :: [Type] -> NonGenericVars -> Copier [Type]
+copyArgs ts ngvars = mapM ct ts
+  where ct t = copyType t ngvars
